c9a79b5730fb9edf81cc638f7a7c446840d6fea6
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2026-04-23 00:20:55 +0800

    Time: 196 ms (52.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 627-swap-sex-of-employees/swap-sex-of-employees.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+UPDATE Salary
+SET sex = CASE WHEN sex = 'm' THEN 'f'
+                WHEN sex = 'f' THEN 'm'
+                ELSE sex
+                END;
