97d80890de6d0b7e1f802606dc115ede34dc8f6b
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2026-04-23 00:06:22 +0800

    Time: 183 ms (87.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT *,
+    CASE WHEN dna_sequence ~ '^ATG.*$' THEN 1 ELSE 0 END AS has_start,
+    CASE WHEN dna_sequence ~ '^.*(TAA|TAG|TGA)$' THEN 1 ELSE 0 END AS has_stop,
+    CASE WHEN dna_sequence ~ '^.*ATAT.*$' THEN 1 ELSE 0 END AS has_atat,
+    CASE WHEN dna_sequence ~ '^.*GGG.*$' THEN 1 ELSE 0 END AS has_ggg
+FROM Samples
+ORDER BY 1
