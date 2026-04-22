e6f3b279b18f5ae6d89ce13061736d89d662a61c
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2026-04-23 00:17:57 +0800

    Time: 230 ms (57.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1926-products-price-for-each-store/products-price-for-each-store.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT product_id,
+    SUM(CASE WHEN store = 'store1' THEN price END) AS store1,
+    SUM(CASE WHEN store = 'store2' THEN price END) AS store2,
+    SUM(CASE WHEN store = 'store3' THEN price END) AS store3
+FROM Products
+GROUP BY product_id
