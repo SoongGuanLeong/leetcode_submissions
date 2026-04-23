37d5b6fec5fbf06c935fa5ead9ba31d38698409f
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2026-04-23 11:34:51 +0800

    Time: 571 ms (31.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1399-page-recommendations/page-recommendations.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT DISTINCT page_id AS recommended_page
+FROM Likes
+WHERE user_id IN
+(SELECT user1_id AS id FROM Friendship
+WHERE user2_id = 1
+UNION
+SELECT user2_id AS id FROM Friendship
+WHERE user1_id = 1)
+
+AND page_id NOT IN (SELECT page_id FROM Likes WHERE user_id = 1)
